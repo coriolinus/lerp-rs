2663363dc9faf05ece033e873fca41c1be8040ac
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2016-08-05 12:35:39 +0200

    Add bounded_lerp, some documentation / tests, continue to tighten up docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,24 @@ extern crate num_traits;
 use std::iter;
 use std::iter::{Iterator, Skip, Chain, Once};
 use std::ops::{Add, Sub, Mul};
-use num_traits::Float;
+use num_traits::{Float, Zero, One};
 
 /// Types which are amenable to linear interpolation and extrapolation.
 ///
 /// This is mainly intended to be useful for complex
 /// numbers, vectors, and other types which may be multiplied by a
 /// scalar while retaining their own type.
+///
+/// It's automatically implemented
+/// for all `T: Copy + Add<Output = T> + Sub<Output = T> + Mul<F, Output = T>`.
 pub trait Lerp<F> {
     /// Interpolate and extrapolate between `self` and `other` using `t` as the parameter.
     ///
     /// At `t == 0.0`, the result is equal to `self`.
     /// At `t == 1.0`, the result is equal to `other`.
     /// At all other points, the result is a mix of `self` and `other`, proportional to `t`.
+    ///
+    /// `t` is unbounded, so extrapolation and negative interpolation are no problem.
     ///
     /// # Examples
     ///
@@ -45,7 +50,44 @@ pub trait Lerp<F> {
     /// # use lerp::Lerp;
     /// assert_eq!(3.0.lerp(4.0, -1.0), 2.0);
     /// ```
+    ///
+    /// Reverse interpolation:
+    ///
+    /// ```
+    /// # use lerp::Lerp;
+    /// assert_eq!(5.0.lerp(3.0, 0.5), 4.0);
+    /// ```
     fn lerp(self, other: Self, t: F) -> Self;
+
+    /// Interpolate between `self` and `other` precisely per the `lerp` function, bounding `t`
+    /// in the inclusive range [0..1].
+    ///
+    /// # Examples
+    ///
+    /// Bounding on numbers greater than one:
+    ///
+    /// ```
+    /// # use lerp::Lerp;
+    /// assert_eq!(3.0.lerp_bounded(4.0, 2.0), 4.0);
+    /// ```
+    ///
+    /// Bounding on numbers less than zero:
+    ///
+    /// ```
+    /// # use lerp::Lerp;
+    /// assert_eq!(3.0.lerp_bounded(5.0, -2.0), 3.0);
+    /// ```
+    fn lerp_bounded(self, other: Self, t: F) -> Self
+        where Self: Sized,
+              F: PartialOrd + Copy + Zero + One
+    {
+        let t = match t {
+            t if t < F::zero() => F::zero(),
+            t if t > F::one() => F::one(),
+            t => t,
+        };
+        self.lerp(other, t)
+    }
 }
 
 /// Types which can construct a lerping iterator from one point to another
